be472d75358527fc37e0e56b3de052d614f9f8ad
juber131 <jubayerahmad131@gmail.com>
2024-05-10 15:31:16 +0600

    add registation

--- src/assets/Route/Router.jsx
+++ src/assets/Route/Router.jsx
@@ -5,6 +5,7 @@ import {
 import ErrorPage from "../../pages/ErrorPage";
 import Home from "../../pages/Home";
 import Login from "../../pages/Login";
+import Registration from "../../pages/Registration";
 
 
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: "/login",
                 element: <Login></Login>,
+            },
+            {
+                path: "/registation",
+                element: <Registration></Registration>,
             },
         ],
     },
